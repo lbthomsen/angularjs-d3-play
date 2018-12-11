8e554c7920a1e53d1dedfe876485c2c169c6f1e9
Lars Boegild Thomsen <lth@netcompartner.com>
2018-12-11 10:24:23 +0800

    One axis drawn

--- app.js
+++ app.js
@@ -27,7 +27,7 @@
         }
     ]);
 
-    app.factory("RoomService", ["$log", "RoomsResource", "SensorsResource", 
+    app.factory("RoomService", ["$log", "RoomsResource", "SensorsResource",
         function ($log, roomsResource, sensorsResource) {
             return {
                 getRooms: function (successCb, errorCb) {
@@ -124,36 +124,83 @@
         function ($log, $scope, $window, $interval, roomService) {
             $log.debug("RoomGraphController: starting");
 
+            var padding = 5;
+
             var interval = null;
             var svg = null;
+            var d3 = null;
 
             var that = this;
-        
+
             that.sensors = $scope.sensors;
             //that.sensorData = {};
 
             // Refresh data - called once per minute but will not be called before sensors and svg element is ready
             that.refreshData = function () {
                 $log.debug("RoomGraphController: refreshData");
-                var data = {};
-                async.eachSeries(that.sensors, function(sensor, doneSensor) {
+                var sensorData = {};
+                async.eachSeries(that.sensors, function (sensor, doneSensor) {
                     $log.debug("RoomGraphController: getting sensor data for sensor = %o", sensor);
-                    roomService.getSensorData(sensor.id, function (sensorData) {
-                        $log.debug("RoomGraphController: got sensor data = %o", sensorData);
-                        data[sensor.id] = sensorData;
+                    roomService.getSensorData(sensor.id, function (sensorDataset) {
+                        $log.debug("RoomGraphController: got sensor data = %o", sensorDataset);
+                        sensorData[sensor.id] = sensorDataset;
                         doneSensor();
                     });
-                }, function() {
-                    $log.debug("RoomGraphController: got data from all sensors = %o", data);
+                }, function () {
+                    $log.debug("RoomGraphController: Got data from all sensors = %o", sensorData);
 
-                    
+                    // Determine min/max values
+                    var xmin = 100000000000000, xmax = 0, ymin = 200, ymax = -200;
+                    Object.keys(sensorData).forEach(function (key) {
+                        $log.debug("RoomGraphController: searching min/max for dataset = %o", key);
+                        sensorData[key].forEach(function (dataPoint) {
+                            if (dataPoint.timestamp < xmin) xmin = dataPoint.timestamp;
+                            if (dataPoint.timestamp > xmax) xmax = dataPoint.timestamp;
+                            if (dataPoint.temperature < ymin) ymin = dataPoint.temperature;
+                            if (dataPoint.temperature > ymax) ymax = dataPoint.temperature;
+                        })
+                    });
+
+                    $log.debug("RoomGraphController: xmin = %o, xmax = %o, ymin = %o, ymax = %o", xmin, xmax, ymin, ymax);
+
+                    var xScale = d3.scaleLinear()
+                        .domain([xmin, xmax])
+                        .range([padding + 5, $scope.svgElement.clientWidth - padding]);
+
+                    var yScale = d3.scaleLinear()
+                        .domain([0, ymax])
+                        .range([$scope.svgElement.clientHeight, 0]);
+
+                    var xAxisGen = d3.axisBottom()
+                        .scale(xScale)
+                        .ticks(5);
+
+                    var yAxisGen = d3.axisLeft()
+                        .scale(yScale)
+                        .ticks(5);
+
+                    svg.append("svg:g")
+                        .attr("class", "x axis")
+                        //.attr("transform", "translate(0,180)")
+                        .call(xAxisGen);
+
+                    svg.append("svg:g")
+                        .attr("class", "y axis")
+                        //.attr("transform", "translate(0,180)")
+                        .call(yAxisGen);
                 });
             }
 
-            $scope.$watch(function() {
-                return $scope.svgElement && $scope.sensors && $scope.sensors.length > 0;
-            }, function(state) {
+            $scope.$watch(function () {
+                // Check all prerequisites for d3 generation
+                return $window.d3 && $scope.svgElement && $scope.sensors && $scope.sensors.length > 0;
+            }, function (state) {
                 $log.debug("RoomGraphController: state = %o", state);
+
+                d3 = $window.d3;
+                svg = d3.select($scope.svgElement)
+                    .style("width", "100%")
+                    .style("border", "1px solid blue");
 
                 interval = $interval(function () {
                     that.refreshData();
